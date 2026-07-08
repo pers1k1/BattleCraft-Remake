cb177bbfa511e2ad8cd17e7bfc2a15dd62a8a01e
pers1k1 <lol333222444@gmail.com>
2026-07-09 00:06:23 +0300

    v8.6.2

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -63,7 +63,7 @@ namespace CustomLauncher
 
         private static readonly HttpClient _httpClient = new() { Timeout = TimeSpan.FromSeconds(10) };
 
-        private const string VER = "8.6.1";
+        private const string VER = "8.6.2";
         private const string MC = "1.20.1";
         private const string FORGE = "47.4.20";
         private const string FULL_ID = MC + "-forge-" + FORGE;
